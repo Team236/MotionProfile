e16aa94718a649ac71860d781af1e2338c8f17f0
Sam Fuchs <samcfuchs@gmail.com>
2016-02-12 10:17:45 -0500

    Added files from 2016-Robot

---
+++ DriveSide.java
@@ -0,0 +1,35 @@
+package motionProfile;
+
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.SpeedController;
+
+/**
+ * 
+ * @author samcf_000
+ * @param _motorA A SpeedController to set speed
+ * @param _motorB A SpeedController to set speed
+ * @param _enc An Encoder to read distance from
+ */
+public class DriveSide implements ProfileSource, ProfileOutput {
+
+	private SpeedController motorA;
+	private SpeedController motorB;
+	private Encoder enc;
+
+	public DriveSide(SpeedController _motorA, SpeedController _motorB, Encoder _enc) {
+		this.motorA = _motorA;
+		this.motorB = _motorB;
+		this.enc = _enc;
+	}
+
+	@Override
+	public void setSpeed(double speed) {
+		motorA.set(speed);
+		motorB.set(speed);
+	}
+
+	@Override
+	public double getDistance() {
+		return enc.getDistance();
+	}
+}
